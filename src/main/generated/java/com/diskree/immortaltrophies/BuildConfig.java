a95ffe226a723e317a01663a1484d51cf74af8e0
diskree <diskreee@gmail.com>
2024-03-19 22:33:55 +0400

    Initial commit

---
+++ src/main/generated/java/com/diskree/immortaltrophies/BuildConfig.java
@@ -0,0 +1,6 @@
+package com.diskree.immortaltrophies;
+
+public class BuildConfig {
+    public static final String MOD_NAME = "ImmortalTrophies";
+    public static final String MOD_ID = "immortaltrophies";
+}
---
+++ src/main/java/com/diskree/immortaltrophies/client/ImmortalTrophies.java
@@ -0,0 +1,10 @@
+package com.diskree.immortaltrophies.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ImmortalTrophies implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
